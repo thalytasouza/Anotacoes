0f9168bc49b97c31bb6e773c822241f98ca5ff68
Thalyta Souza <mayerflot@gmail.com>
2023-11-22 13:54:07 -0300

    Initial Commit

---
+++ Minhas Anotações/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Minhas Anotações
+//
+//  Created by Thalytinha on 22/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
